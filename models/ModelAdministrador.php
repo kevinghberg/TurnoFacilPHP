a300cbfabb7ac1f647e13a6b4efee8365ff78a7e
gustavosierra <gustavodavid04@gmail.com>
2022-07-03 21:04:46 -0300

    Borrado ModelTurnos, update modeadmin

--- models/ModelAdministrador.php
+++ models/ModelAdministrador.php
@@ -1,8 +1,8 @@
 <?php
-require_once('Model.php');
+require_once('ModelDB.php');
 
 //Las clases siempre empiezan con mayusculas
-class ModelAdministrador extends Model
+class ModelAdministrador extends ModelDB
 {
     
 
